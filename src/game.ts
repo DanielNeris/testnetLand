ba97098b0e0b77aac45ce7f74df04ac7fec58e58
victoriacassolo <vitoria.cassolo@gmail.com>
2022-02-21 14:10:27 -0300

    first commit

---
+++ src/game.ts
@@ -0,0 +1,80 @@
+import * as utils from "@dcl/ecs-scene-utils"
+import * as EthereumController from "@decentraland/EthereumController"
+import { getUserAccount } from "@decentraland/EthereumController"
+
+const messageToSign = `# DCL Signed message
+Attacker: 10
+Defender: 123
+Timestamp: 1512345678`
+
+let eth = EthereumController
+
+executeTask(async () => {
+  const convertedMessage = await eth.convertMessageToObject(messageToSign)
+  const { message, signature } = await eth.signMessage(convertedMessage)
+  log({ message, signature })
+})
+
+
+class RotatorSystem {
+  // this group will contain every entity that has a Transform component
+  group = engine.getComponentGroup(Transform)
+
+  update(dt: number) {
+    // iterate over the entities of the group
+    for (let entity of this.group.entities) {
+      // get the Transform component of the entity
+      const transform = entity.getComponent(Transform)
+
+      // mutate the rotation
+      transform.rotate(Vector3.Up(), dt * 10)
+    }
+  }
+}
+
+// Add a new instance of the system to the engine
+engine.addSystem(new RotatorSystem())
+
+/// --- Spawner function ---
+
+function spawnCube(x: number, y: number, z: number) {
+  // create the entity
+  const cube = new Entity()
+
+  // add a transform to the entity
+  cube.addComponent(new Transform({ position: new Vector3(x, y, z) }))
+
+  // add a shape to the entity
+  cube.addComponent(new BoxShape())
+
+  // add the entity to the engine
+  engine.addEntity(cube)
+
+  return cube
+}
+
+/// --- Spawn a cube ---
+
+const cube = spawnCube(8, 1, 8)
+
+cube.addComponent(
+  new OnPointerDown(() => {
+    cube.getComponent(Transform).scale.z *= 1.1
+    cube.getComponent(Transform).scale.x *= 0.9
+
+    spawnCube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
+  })
+)
+
+let avocado = new Entity()
+
+avocado.addComponent(new GLTFShape("models/avocado.gltf"))
+avocado.addComponent(
+  new Transform({
+    position: new Vector3(3, 1, 3),
+    scale: new Vector3(10, 10, 10),
+  })
+)
+engine.addEntity(avocado)
+
+
